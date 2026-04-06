3d891bca1a2ca70d5d6089be87d2eb57b2ea7620
Yael <yaelgustavof@gmail.com>
2026-04-06 16:17:14 -0600

    Avanze del algoritmo de resta bien mandado

---
+++ main.c
@@ -0,0 +1,11 @@
+#include "operaciones.h"
+
+typedef union {
+    uint32_t un;
+    float fl;
+} FloatConversion;
+
+int main() {
+
+    return 0;
+}
---
+++ operaciones.h
@@ -0,0 +1,9 @@
+#ifndef OPERACIONES_H
+#define OPERACIONES_H
+
+#include <stdio.h>
+#include <stdint.h>
+
+uint32_t restaF(uint32_t nu1, uint32_t nu2);
+
+#endif
