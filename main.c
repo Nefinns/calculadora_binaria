45811dee9d8c7de40ddbb28379fe56b63412b2b0
Nefinns <nefinnstars@gmail.com>
2026-04-06 19:28:14 -0600

    Estructura inicial: main y cabeceras de operaciones

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "operaciones.h"
+
+int main()
+{
+    mostrar_resultado_suma();
+    return 0;
+}
---
+++ operaciones.h
@@ -0,0 +1,9 @@
+#ifndef OPERACIONES_INCLUDED
+#define OPERACIONES_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
+
+float sumador_bits(float a,float b);
+void mostrar_resultado_suma();
+
+#endif
