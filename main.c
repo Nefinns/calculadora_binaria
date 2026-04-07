a5ba8938d951944a9ba95ea12fba50f018e0eb13
Yael <yaelgustavof@gmail.com>
2026-04-06 18:59:08 -0600

    final final ahora asi

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "operaciones.h"
 
 typedef union {
@@ -6,6 +7,20 @@ typedef union {
 } FloatConversion;
 
 int main() {
+    FloatConversion n1, n2, res;
 
+    printf(" Restador de Punto Flotante IEEE 754 \n\n");
+    
+    printf("Ingresa el primer numero: ");
+    scanf("%f", &n1.fl);
+    
+    printf("Ingresa el segundo numero: ");
+    scanf("%f", &n2.fl);
+
+    res.un = restaF(n1.un, n2.un);
+
+    printf("\n Resta de %f - %f:\n", n1.fl, n2.fl);
+    printf("Resultado : %f\n", res.fl);
+    
     return 0;
 }
